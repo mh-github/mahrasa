a718bfa297d1968b95da9f0eecafd80052bbbeb9
Mahboob Hussain <mahboob.h06@gmail.com>
2021-07-07 01:55:46 +0530

    upgraded to ruby 2.6.6 and rails 6.1.4. removed all gem versions in Gemfile, except ruby

--- db/migrate/20170213121342_add_reference_order_to_line_item.rb
+++ db/migrate/20170213121342_add_reference_order_to_line_item.rb
@@ -1,4 +1,4 @@
-class AddReferenceOrderToLineItem < ActiveRecord::Migration
+class AddReferenceOrderToLineItem < ActiveRecord::Migration[6.0]
   def change
     add_reference   :line_items, :order, index: true
     add_foreign_key :line_items, :orders
